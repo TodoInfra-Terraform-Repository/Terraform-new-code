4905ece584c71e6433ee232f1789926934df6cf2
SN262024 <surendrachauhan18@gmail.com>
2025-08-04 23:55:53 +0530

    Added ner resource group and storage account

---
+++ Resource_group/main.tf
@@ -0,0 +1,28 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.38.1"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = "c316f505-7597-4175-b5db-d2949009d506"
+
+}
+
+resource "azurerm_resource_group" "RG" {
+  name     = "rg-surendra1"
+  location = "West Europe"
+}
+resource "azurerm_storage_account" "Stg" {
+  depends_on               = [azurerm_resource_group.RG]
+  name                     = "storagsurendra1"
+  resource_group_name      = "rg-surendra1"
+  location                 = "West Europe"
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+
+}
